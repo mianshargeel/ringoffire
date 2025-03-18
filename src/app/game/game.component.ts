afc6f944315b739ea7b472aeec61d9293055ed91
mian shargeel <call2poma@gmail.com>
2025-03-18 14:40:35 +0100

    Showing Card and added them Animation.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-
+  pickCardAnimation: boolean = false;
+  
+  constructor() { }
+  
+  pickCard() {
+    this.pickCardAnimation = true;
+  }
 }
